6e76fdccf5b108662a7177a5cab3bf3b60954c56
Alexandre Roux <alex@tekartik.com>
2018-10-26 21:36:12 +0200

    test: remove test on dartdevk

--- test/dartdevk_test.dart
+++ test/dartdevk_test.dart
@@ -9,13 +9,13 @@ void main() {
   group('dartdevk', () {
     test('help', () async {
       ProcessResult result = await runCmd(DartDevkCmd(['--help']));
-      expect(result.stdout, contains("Usage: dartdevk"));
+      //expect(result.stdout, contains("Usage: dartdevk"));
       expect(result.exitCode, 0);
     });
     // version not supported yet
     test('version', () async {
       ProcessResult result = await await runCmd(DartDevkCmd(['--version']));
-      expect(result.stdout, contains("dartdevk"));
+      //expect(result.stdout, contains("dartdevk"));
       expect(result.exitCode, 0);
     }, skip: true);
   });
