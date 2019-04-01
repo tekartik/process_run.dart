8e0efe2520e8cc20d24929c6eab7df88cd103aef
alex <alex@tekartik.com>
2019-04-01 10:34:15 +0200

    test: skip test with dependency on example with build_runner

--- tool/travis.dart
+++ tool/travis.dart
@@ -9,6 +9,7 @@ dartanalyzer --fatal-warnings --fatal-infos example lib tool test
 
 # Run tests
 pub run test -p vm -r expanded
+pub run test -p vm -r expanded test/example_hex_utils_test_.dart
 
 # Run tests using build_runner
 pub run build_runner test -- -p vm -r expanded
