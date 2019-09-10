f7e4867ba4f2ffb2be25f0dfa1249e08d4ba79e3
alex <alex@tekartik.com>
2019-09-10 16:49:01 +0200

    feat: add getFlutterVersion

--- lib/src/flutterbin_cmd.dart
+++ lib/src/flutterbin_cmd.dart
@@ -1,5 +1,10 @@
-import 'package:process_run/src/which.dart';
+import 'dart:convert';
+
+import 'package:process_run/cmd_run.dart';
 import 'package:process_run/process_run.dart';
+import 'package:process_run/src/which.dart';
+import 'package:pub_semver/pub_semver.dart';
+
 import 'process_cmd.dart';
 
 String _flutterExecutablePath;
@@ -24,4 +29,31 @@ class FlutterCmd extends ProcessCmd {
 
   @override
   String toString() => executableArgumentsToString('flutter', arguments);
+}
+
+/// Parse flutter version
+Future<Version> getFlutterVersion() async {
+  // $ flutter --version
+  // Flutter 1.7.8+hotfix.4 • channel stable • https://github.com/flutter/flutter.git
+  // Framework • revision 20e59316b8 (8 weeks ago) • 2019-07-18 20:04:33 -0700
+  // Engine • revision fee001c93f
+  // Tools • Dart 2.4.0
+  var cmd = FlutterCmd(['--version']);
+  var output = LineSplitter.split((await runCmd(cmd)).stdout.toString())
+      .join(' ')
+      .split(" ")
+      .map((word) => word?.trim())
+      .where((word) => word?.isNotEmpty ?? false);
+  bool foundFlutter = false;
+  try {
+    for (var word in output) {
+      if (foundFlutter) {
+        return Version.parse(word);
+      }
+      if (word.toLowerCase().contains('flutter')) {
+        foundFlutter = true;
+      }
+    }
+  } catch (_) {}
+  return null;
 }
--- test/flutterbin_cmd_test.dart
+++ test/flutterbin_cmd_test.dart
@@ -3,6 +3,7 @@ library process_run.flutterbin_cmd_test;
 
 import 'dart:io';
 
+import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart';
 import 'package:process_run/cmd_run.dart';
@@ -12,7 +13,7 @@ import 'package:process_run/which.dart';
 
 void main() {
   group('flutterbin_cmd', () {
-    test('version', () async {
+    test('run_version', () async {
       //print(flutterExecutablePath);
       ProcessCmd cmd = FlutterCmd(['--version']);
       // expect(cmd.executable, flutterExecutablePath);
@@ -21,6 +22,11 @@ void main() {
       expect(result.stdout.toLowerCase(), contains("dart"));
       expect(result.stdout.toLowerCase(), contains("revision"));
       expect(result.stdout.toLowerCase(), contains("flutter"));
+    });
+
+    test('get version', () async {
+      var version = await getFlutterVersion();
+      expect(version, greaterThan(Version(1, 5, 0)));
     });
 
     test('which', () {
