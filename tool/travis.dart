2502b3bbae4fb5f8a382dffb332b5a5db4ab52eb
alex <alex@tekartik.com>
2021-03-22 09:27:22 +0100

    fix travis

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,28 +1,16 @@
-import 'dart:io';
-
 import 'package:process_run/shell.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
   var shell = Shell();
-
-  // Formatting change in 2.9 with hashbang first line
-  if (dartVersion >= Version(2, 9, 0, pre: '0')) {
-    await shell.run('''
-    # Formatting
-    dartfmt -n --set-exit-if-changed .
-    ''');
-  }
-
-  print(Platform.version);
   await shell.run('''
+# Format
+dart format --set-exit-if-changed -o none .
+
 # Analyze code
-dartanalyzer --fatal-warnings --fatal-infos example lib tool test
+dart analyze --fatal-warnings --fatal-infos .
 
 # Run tests
 pub run test -p vm -r expanded
-pub run test -p vm -r expanded test/example_hex_utils_test_.dart
 
 # Run tests using build_runner
 pub run build_runner test -- -p vm -r expanded
