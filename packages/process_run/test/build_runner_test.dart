215b21405add53ecb6ea5bc6c4cf30286fd389f4
Alexandre Roux <alex@tekartik.com>
2023-04-20 16:34:07 +0200

    test: remove obsolete webdev test

--- packages/process_run/test/build_runner_test.dart
+++ packages/process_run/test/build_runner_test.dart
@@ -12,15 +12,6 @@ void defineTests() {
       final result = await runCmd(PbrCmd(['--help']));
       expect(result.exitCode, 0);
       expect(result.stdout, contains('Usage: build_runner'));
-
-      /*
-      this does not work when ran from IDE
-      // The raw version is displayed
-      result = await runCmd(WebDevCmd(['--version'])..runInShell = true);
-      var version = Version.parse((result.stdout as String).trim());
-      expect(version, greaterThan(Version(1, 0, 0)));
-      expect(result.exitCode, 0);
-      */
     });
   });
 }
