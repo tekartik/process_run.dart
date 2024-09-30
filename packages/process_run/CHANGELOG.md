2437100e9a3a758599f6e14448feb7de549ee61d
Alexandre Roux <alex@tekartik.com>
2024-09-30 18:04:00 +0200

    [process_run] v1.2.0+3 fix tests

--- packages/process_run/CHANGELOG.md
+++ packages/process_run/CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.2.0+2
+## 1.2.0+3
 
 * Export ShellBinCommand
 * requires dart 3.5
--- packages/process_run/test/src/compile_shell.dart
+++ packages/process_run/test/src/compile_shell.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:process_run/shell.dart';
+import 'package:process_run/src/bin/shell/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 /// Return the executable path.
 Future<String> compileShellBin({bool force = false}) async {
@@ -11,6 +13,18 @@ Future<String> compileShellBin({bool force = false}) async {
   var dsExePath = join('build', folder, 'ds$exeExtension');
   var dsExeDir = dirname(dsExePath);
   var shell = Shell(verbose: false);
+  if (File(dsExePath).existsSync()) {
+    try {
+      var output = (await shell.run('$dsExePath --version')).outText.trim();
+      if (Version.parse(output) != shellBinVersion) {
+        force = true;
+      }
+    } catch (_) {
+      // ignore
+      force = true;
+    }
+  }
+
   if (!File(dsExePath).existsSync() || force) {
     Directory(dsExeDir).createSync(recursive: true);
     await shell.run(
