2ef8a924111335963623d8e779e1f9c09ccf7c40
Alexandre Roux <alex@tekartik.com>
2025-02-04 16:13:36 +0100

    simple demo

--- packages/process_run/example/demo.dart
+++ packages/process_run/example/demo.dart
@@ -4,14 +4,14 @@ import 'dart:io';
 import 'package:process_run/process_run.dart';
 
 Future main() async {
-  // Run the command
-  await runExecutableArguments('echo', ['hello world']);
-
-  // Stream the out to stdout
-  await runExecutableArguments('echo', ['hello world']);
-
   // Calling dart
   await runExecutableArguments('dart', ['--version'], verbose: true);
+
+  // Stream the out to stdout
+  await runExecutableArguments('echo', ['hello world'], verbose: true);
+
+  // Run the command
+  await runExecutableArguments('echo', ['hello world']);
 
   // stream the output to stderr
   await runExecutableArguments('dart', ['--version'], stderr: stderr);
