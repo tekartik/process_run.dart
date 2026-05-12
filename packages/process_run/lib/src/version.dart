85912e1b55bce2d7de27427859ba14efc4a2870b
Alexandre Roux <alex@tekartik.com>
2026-05-12 19:06:35 +0200

    bump version to 1.3.3

--- packages/process_run/lib/src/version.dart
+++ packages/process_run/lib/src/version.dart
@@ -4,7 +4,7 @@ library;
 import 'package:pub_semver/pub_semver.dart';
 
 /// Package version text
-const packageVersionText = '1.3.3-2';
+const packageVersionText = '1.3.3';
 
 /// Package version
 final packageVersion = Version.parse(packageVersionText);
