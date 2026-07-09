cf8550d220857c367d3ece9dc4b379929a464aa8
Alexandre Roux <alex@tekartik.com>
2026-07-09 21:19:02 +0200

    feat: update version to 1.3.4+1 and enhance verbose flag handling in ShellBinCommand

--- packages/process_run/lib/src/version.dart
+++ packages/process_run/lib/src/version.dart
@@ -4,7 +4,7 @@ library;
 import 'package:pub_semver/pub_semver.dart';
 
 /// Package version text
-const packageVersionText = '1.3.4';
+const packageVersionText = '1.3.4+1';
 
 /// Package version
 final packageVersion = Version.parse(packageVersionText);
